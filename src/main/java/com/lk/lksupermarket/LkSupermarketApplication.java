5876a4f8c33d324417b26a5dc08d7acce07b64a0
gxf <gitee972915gxf>
2025-07-07 11:44:19 +0800

    fix-first commit

---
+++ src/main/java/com/lk/lksupermarket/LkSupermarketApplication.java
@@ -0,0 +1,13 @@
+package com.lk.lksupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkSupermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LkSupermarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lk/lksupermarket/LkSupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.lksupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LkSupermarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
